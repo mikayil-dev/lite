321d0cf29849d3fbfd837098b831b5b7e6ca8a09
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-07-06 13:28:08 +0200

    format and settings

---
+++ .prettierrc.ts
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+export default {
+	tabWidth: 4,
+	useTabs: true,
+	semi: true,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSpacing: true,
+	bracketSameLine: false,
+	arrowParens: 'avoid',
+	plugins: ['prettier-plugin-svelte'],
+	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
+};
