4291192f7ccf1eae348413f9325e4c9c9449acb2
Mikayil Gacek <mikayil_gacek@icloud.com>
2025-07-21 22:16:49 +0200

    focus styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,16 @@
 	let { children } = $props();
 </script>
 
+<svelte:body
+	onkeydown={e => {
+		if (e.key !== 'Tab') return;
+		document.body.setAttribute('data-usingkeyboard', '');
+	}}
+	onclick={() => {
+		document.body.removeAttribute('data-usingkeyboard');
+	}}
+/>
+
 <Header />
 
 {@render children?.()}
